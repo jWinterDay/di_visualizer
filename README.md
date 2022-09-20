8bcab614bf9df5c6603c7842e3aad6a252150303
jwd25 <jwinterday@mail.ru>
2022-09-20 16:20:27 +0500

    init

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ di_visualizer \
     di \
     --directory lib/src \
     --output uml.txt
+
+## info
+tool for visualization your di dependencies
+
+use @diService annotation for your class
+and @diInject for fields in your service
+
+and get uml diagram
---
+++ example/example.dart
@@ -0,0 +1,43 @@
+import 'package:di_visualizer_annotation/di_visualizer_annotation.dart';
+
+@diService
+class Service1 {
+  Service1(this.util, this.repository1);
+
+  @diInject
+  final Util util;
+
+  @diInject
+  final Repository1 repository1;
+}
+
+@diService
+class Service2 {
+  Service2(this.util, this.repository2);
+
+  @diInject
+  final Util util;
+
+  @diInject
+  final Repository2 repository2;
+}
+
+@diService
+class Util {}
+
+@diService
+class RepoUtil {}
+
+@diService
+class Repository1 {
+  Repository1(this.repoUtil);
+
+  final RepoUtil repoUtil;
+}
+
+@diService
+class Repository2 {
+  Repository2(this.repoUtil);
+
+  final RepoUtil repoUtil;
+}
